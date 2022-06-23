822f7bb3cca8dfd78f123bf239e35cab5b10b453
phpjscoding <phpjscoding@gmail.com>
2022-06-24 00:17:50 +0100

    35/100

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
+import React from "react"
+import { Header } from "../components"
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy"
+import { Table } from "antd"
 const Orders = () => {
-  return <div>Orders</div>;
-};
+  return (
+    <div>
+      <Header category="order" title="Orders" />
+      {ordersGrid.map((item, index) => (
+        <p key={index}> {console.log(item)} </p>
+      ))}
+    </div>
+  )
+}
 
-export default Orders;
+export default Orders
